681605d957126f416715778724894f6c19460cb1
Cristian Nozzi <cristian.nozzi@hybrida.io>
2022-03-02 11:22:49 +0100

    Modifiche grafiche

--- Demo_streamlit.py
+++ Demo_streamlit.py
@@ -3,63 +3,82 @@ import pandas as pd
 import seaborn as sns
 from joblib import load
 import plotly.graph_objects as go
+from PIL import Image
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    print('loading model')
-    return load('Rete_Neurale_Bialetti.joblib')
+    print('Loading model...')
 
+    return load('Rete_Neurale_Bialetti.joblib')
 
 @st.cache()
 def load_data():
-    print('loading data')
+    print('Loading data...')
     df = pd.read_csv('Df_Bialetti.csv')
-    return df
 
+    return df
 
 model = load_model()
 df = load_data()
 
-st.title('Welcome to Cassandra')
+st.title('Cassandra - Scenario Simulator')
 
+st.sidebar.image(Image.open(f"./references/logo.png"), width=200)
+st.sidebar.title('1. Seasonal Variables')
 
 user_input= {}
-
-
-
-
 categorical = ['day_week', 'month']
 
 for feat in categorical:
     unique_values = df[feat].unique()
-    user_input[feat]=st.sidebar.selectbox(feat, unique_values)
+    unique_values.sort()
 
-numerical = ['Spesa Facebook','google','Organico']
+    label = ""
+    if feat == "day_week":
+        label = "Day of the week"
+    elif feat == 'month':
+        label = "Month"
+
+    if feat == 'day_week':
+        display = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    elif feat == 'month':
+        display = ("Si", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dicember")
+
+    user_input[feat] = st.sidebar.selectbox(label, unique_values, format_func=lambda x: display[x])
+
+st.sidebar.title('2. Media Variables')
+
+numerical = ['Spesa Facebook', 'google', 'Organico']
 
 for feat in numerical:
     v_min = float(df[feat].min())
     v_max =float(df[feat].max())
+
+    label = ""
+    if feat == "Spesa Facebook":
+        label = "Facebook Ads Spend"
+    elif feat == "google":
+        label = "Google Ads Spend"
+    else:
+        label = "Organic Sessions"
+
     user_input[feat]=st.sidebar.slider(
-        feat,
-        min_value= v_min,
-        max_value=v_max,
-        value= (v_min+v_max)/2
+        label,
+        min_value = v_min,
+        max_value = v_max,
+        value = (v_min+v_max)/2
     )
 X = pd.DataFrame([user_input])
-st.write(X)
-
-
+#st.write(X)
 
 z=(X['Spesa Facebook']+X['google'])
-
 cpa= z/model.predict(X)
 
 prediction =model.predict(X)
 
-st.title('Previsione transazioni')
+st.title('Predicted sales')
 
-
-fig= go.Figure(
+fig = go.Figure(
     go.Indicator(
         mode= 'gauge+number',
         value=prediction[0]
@@ -67,36 +86,11 @@ fig= go.Figure(
 )
 
 st.plotly_chart(fig)
-st.write(prediction)
-fig_cpa= go.Figure(
-    go.Indicator(
-        mode= 'number',
-        value= cpa[0]
-    )
-)
-st.title('CPA')
-st.plotly_chart(fig_cpa)
 
 aov= 60
-
 fatturato=model.predict(X)*aov
-
 profit=fatturato*0.6-z
 
-
-fig_profit= go.Figure(
-    go.Indicator(
-        mode= 'number',
-        value= profit[0]
-    )
-)
-st.subheader('Profit')
-st.plotly_chart(fig_profit)
-
-
-
-
-
-
-
-
+st.subheader("Revenue: " + str(round(fatturato[0], 2)) + " $")
+st.subheader("Profit: " + str(round(profit[0], 2)) + " $")
+st.subheader("CPA: " + str(round(cpa[0], 2)) + " $")
